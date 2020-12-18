fec7b22d0dc8bbf833f572c8ed555380b8f0ae44
Joel Parkinson <joel.tp@hotmail.co.uk>
2020-12-18 23:05:56 +0000

    Completed Day 3 part 2

--- solutions/day3.py
+++ solutions/day3.py
@@ -1,3 +1,4 @@
+#Read and format data from input file
 def readFile(location):
     input = []
     file = open(location)
@@ -9,6 +10,8 @@ def readFile(location):
         input.append(chars)
     return input
 
+#Toboggan function moves the character by a specified number of units in the x and y directions
+#The function returns the number of 'tree' tiles encountered 
 def Toboggan(map, deltaX, deltaY):
     treeCount = 0
     count = 0
@@ -28,5 +31,10 @@ def Toboggan(map, deltaX, deltaY):
 
 input = readFile("./inputs/day3.txt")
 print(input)
-output = Toboggan(input, 3, 1)
-print(output)
+output1 = Toboggan(input, 3, 1)
+print(output1)
+
+#Part 2
+#Generalised inital function used in part 1 so that any change in x and y can be easily calculated
+output2 = Toboggan(input, 1, 1) * Toboggan(input, 3,1 ) * Toboggan(input, 5, 1) * Toboggan(input, 7, 1) *  Toboggan(input, 1, 2)
+print(output2)
