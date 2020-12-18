dea08ceeefc497d324b5c2f9399e8ca9f803e5b0
Joel Parkinson <joel.tp@hotmail.co.uk>
2020-12-18 22:22:28 +0000

    Completed Day 2 part 2

--- solutions/day2.py
+++ solutions/day2.py
@@ -9,24 +9,53 @@ def readFile(location):
     return input
 
 #Count number of valid passwords according to rules explained in the question
-#Check if the password contains the legal amount of the specified character
-def countValidPasswords(passList):
+#Can be setup to work for part 1 or part 2
+def countValidPasswords(passList, type= 1):
     validPassCount = 0
-    for passSet in passList:
-        count = 0
+    if(type==1):
+        validateFunc = countValidChar
+    if(type==2):
+        validateFunc = checkValidPos
+    for passSet in passList:    
         policy = passSet[0]
         password = passSet[1]
         [passCount, passChar] = policy.split(" ")
         [lower, upper] = passCount.split("-")
-        for char in password:
-            if(char == passChar):
-                count+=1
-        if(count >= int(lower) and count <= int(upper)):
-            validPassCount+=1
+        validPassCount += validateFunc(lower, upper, passChar, password)
+
     return validPassCount
+
+#Helper function for part 1
+#Check if the password contains the legal amount of the specified character
+def countValidChar(lower, upper, passChar, password):
+    count = 0
+    for char in password:
+        if(char == passChar):
+            count+=1
+    if(count >= int(lower) and count <= int(upper)):
+        return 1
+    return 0
+
+#Helper function for part 2
+#Checks that the specified char appears in one and only one of the given positions
+def checkValidPos(lower, upper, passChar, password):
+    lowerPos = int(lower)
+    upperPos = int(upper)
+    if(password[lowerPos] == passChar and password[upperPos]== passChar):
+        return 0
+    elif(password[lowerPos] == passChar or password[upperPos]== passChar):
+        return 1
+    return 0
 
 #Part 1
 #Made use of seperators in the strings to divide input into computable chunks 
 input = readFile("./inputs/day2.txt")
-output = countValidPasswords(input)
-print(output)
+output = countValidPasswords(input, 1)
+print("The answer to part 1 is: " + str(output))
+
+#Part 2
+#For part 2, the function used in part one was repurposed so that it could be used for both parts
+#The char counting section of part 1 was split off into the helper function countValidChar
+#Meanwhile, the new position check for part 2 was added in the helper function checkValidPos
+output = countValidPasswords(input, 2)
+print("The answer to part 2 is: " + str(output))
